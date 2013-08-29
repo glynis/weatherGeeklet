6df6311c0ed787e73c132aa151bcfeb1bd0551f4
Glynis MacMillan <glynis.macmillan@gmail.com>
2013-08-28 20:43:47 -0400

    Created repository

---
+++ weather.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASE_DIR="/Users/glynis/Pictures/wallpapers/geektoolNYC/"
+cd "$BASE_DIR"
+
+while true
+do
+	curl -s http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=BDL > weather.xml
+#	weather=$(cat weather.xml | grep '<icon>' | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/')
+	weather=partlycloudy
+	file="$weather.png"
+	if [ -s "current.png" ]; then
+	rm current.png
+	fi
+	cp $file current.png
+	sleep 3600
+done
+# clear, partlycloudy, mostlycloudy, rain
