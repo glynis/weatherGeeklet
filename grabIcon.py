b82058e50fc9133548089a199f06554150341b36
Glynis MacMillan <glynis.macmillan@gmail.com>
2013-10-16 13:32:17 -0400

    first commit

---
+++ grabIcon.py
@@ -0,0 +1,6 @@
+import sys
+
+iconString = sys.stdin.read()
+
+print(iconString[6:-9])
+
--- weather.sh
+++ weather.sh
@@ -6,14 +6,15 @@ cd "$BASE_DIR"
 while true
 do
 	curl -s http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=BDL > weather.xml
+	weather=$(cat weather.xml | grep '<icon>' | tr -d '\t' | python grabIcon.py )
 #	weather=$(cat weather.xml | grep '<icon>' | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/')
-	weather=cloudy
+#	weather=cloudy
+	echo $weather
 	file="$weather.png"
 	if [ -s "current.png" ]; then
 	rm current.png
 	fi
 	cp $file current.png
-	sleep 3600
+	sleep 300
 done
 # clear, partlycloudy, mostlycloudy, rain
-
